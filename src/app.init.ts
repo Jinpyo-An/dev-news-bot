84acaba5d4b27bcf270b24d2758dfe22d3e7fd0f
Jinpyo-Ahn <dkswlsvy3312@gmail.com>
2025-02-18 13:52:02 +0900

    Modify main code

--- src/app.init.ts
+++ src/app.init.ts
@@ -1,7 +1,39 @@
-import {RSSParseFeed} from "@rss_parser/rssFeedParser.ts";
+import {Client, GatewayIntentBits, Events, TextChannel} from 'discord.js';
+import 'dotenv/config';
+import * as process from "node:process";
+import {scheduleRSSFeed} from "@/utils/cron.ts";
 
-const feedUrl = "https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US:en";
+const botToken = process.env.BOT_TOKEN;
+const channelId = process.env.CHANNEL_ID;
 
-RSSParseFeed(feedUrl);
+if (!botToken) throw new Error("BOT_TOKEN 값을 불러오지 못했습니다.");
 
+if (!channelId) throw new Error("CHANNEL_ID 값을 불러오지 못했습니다.");
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildIntegrations,
+  ],
+});
+
+client.once(Events.ClientReady, async (c) => {
+  console.log(`${c.user.tag}에 로그인 했습니다.`);
+
+  try {
+    const channel = await client.channels.fetch(channelId as string) as TextChannel;
+    if (!channel || !(channel instanceof TextChannel)) {
+      console.error(`ID가 ${channelId}인 채널을 찾을 수 없거나, 텍스트 채널이 아닙니다.`);
+      return;
+    }
+
+    scheduleRSSFeed(channel);
+
+  } catch (error) {
+    console.error('채널 가져오기 오류:', error);
+  }
+});
+
+client.login(botToken);
--- src/rss_parser/rssFeedParser.ts
+++ src/rss_parser/rssFeedParser.ts
@@ -1,30 +1,53 @@
 import Parser from 'rss-parser';
+import {TextChannel} from "discord.js";
 
 const parser = new Parser();
 
+
+
+// Published Date Formatting
 function formatDate(date: Date): string {
   const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1
+  const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
   const hours = String(date.getHours()).padStart(2, '0');
-  return `${year}년${month}월${day}일${hours}시`;
+  return `${year}년 ${month}월 ${day}일 ${hours}시`;
 }
 
-// RSS 피드 파싱 및 게시물 저장
-export async function RSSParseFeed(url: string): Promise<void> {
-  const now = new Date().getTime(); // 현재 시간
+// RSS Feed Parsing
+export async function RSSParseFeed(url: string, channel: TextChannel): Promise<void> {
+  const now = new Date().getTime();
+  const posts: string[] = [];
 
   try {
     const feed = await parser.parseURL(url);
 
+    if (!feed.items) {
+      console.warn("피드에 아이템이 없습니다.", url);
+      return;
+    }
+
     feed.items.forEach((item: any) => {
-      if (now - new Date(item.pubDate).getTime() < 60 * 60 * 24 * 1000) {
-        console.info(`발행일: ${formatDate(new Date(item.pubDate))}\n제목: ${item.title}\n링크: ${item.link}`);
+      const pubDate = new Date(item.pubDate);
+      const pubDateFormatted = formatDate(pubDate);
+
+      if (now - pubDate.getTime() < 60 * 60 * 24 * 1000) {
+        posts.push(`발행일: ${pubDateFormatted}\n제목: ${item.title}\n링크: ${item.link}`);
       }
     });
+
   } catch (error) {
     console.error('RSS 피드 파싱 중 오류가 발생했습니다: ' + error);
-    throw new Error('RSS 피드 파싱 실패');
+    await channel.send("RSS 피드 파싱 중 오류가 발생했습니다.")
+  }
+
+  if (posts.length > 0) {
+    for (const post of posts) {
+      await channel.send(post);
+    }
+  } else {
+    console.info("업데이트된 게시물이 없습니다.");
+    await channel.send("업데이트된 게시물이 없습니다.");
   }
 }
 
