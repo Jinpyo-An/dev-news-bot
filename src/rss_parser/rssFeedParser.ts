0882dbbc1fbef8367c5679e0d78dfc59a4b7e305
Jinpyo-Ahn <dkswlsvy3312@gmail.com>
2025-02-24 17:45:36 +0900

    update 일 -> 연/월/일 비교 로직으로 수정

--- src/rss_parser/rssFeedParser.ts
+++ src/rss_parser/rssFeedParser.ts
@@ -6,47 +6,52 @@ const parser = new Parser();
 // RSS Feed Parsing
 export async function RSSParseFeed(url: string, channel: TextChannel): Promise<void> {
 
-    const now = new Date().getDate();
-    const yesterdayDate = now - 1;
+  const now = new Date();
+  const yesterday = new Date(now);
+  yesterday.setDate(now.getDate() - 1);
 
-    let posts: string[] = [];
+  let posts: string[] = [];
 
-    try {
-        const feed = await parser.parseURL(url);
+  try {
+    const feed = await parser.parseURL(url);
 
-        if (!feed.items) {
-            console.warn("No Items in Feed", url);
-            return;
-        }
-
-        feed.items.forEach((item: Item) => {
-
-            if (item.pubDate && !isNaN(Date.parse(item.pubDate))) {
-                const pubDate = new Date(item.pubDate);
-
-                if (yesterdayDate == pubDate.getDate()) {
-                    posts.push(`${item.title}\n${item.link}\n`);
-                }
-            } else {
-                console.warn("Invalid or missing pubDate", item.pubDate);
-            }
-        });
-
-    } catch (error) {
-        console.error('RSS 피드 파싱 중 오류가 발생했습니다: ' + error);
-        await channel.send("RSS 피드 파싱 중 오류가 발생했습니다.")
+    if (!feed.items) {
+      console.warn("No Items in Feed", url);
+      return;
     }
 
-    if (posts.length > 0) {
-        for (const post of posts) {
-            try {
-                await channel.send(post);
-            } catch (sendError) {
-                console.error("Discord 채널에 메시지 전송 중 오류 발생:", sendError);
-            }
+    feed.items.forEach((item: Item) => {
+
+      if (item.pubDate && !isNaN(Date.parse(item.pubDate))) {
+        const pubDate = new Date(item.pubDate);
+
+        if (
+          yesterday.getFullYear() === pubDate.getFullYear() &&
+          yesterday.getMonth() === pubDate.getMonth() &&
+          yesterday.getDate() === pubDate.getDate()
+        ) {
+          posts.push(`${item.title}\n${item.link}\n`);
         }
-    } else {
-        console.info("업데이트된 게시물이 없습니다.");
+      } else {
+        console.warn("Invalid or missing pubDate", item.pubDate);
+      }
+    });
+
+  } catch (error) {
+    console.error('RSS 피드 파싱 중 오류가 발생했습니다: ' + error);
+    await channel.send("RSS 피드 파싱 중 오류가 발생했습니다.")
+  }
+
+  if (posts.length > 0) {
+    for (const post of posts) {
+      try {
+        await channel.send(post);
+      } catch (sendError) {
+        console.error("Discord 채널에 메시지 전송 중 오류 발생:", sendError);
+      }
     }
+  } else {
+    console.info("업데이트된 게시물이 없습니다.");
+  }
 }
 
